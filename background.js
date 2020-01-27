df8db9337b4710fa0d7ef1618511a167f8521c9e
CrisSugar <aguacero18@hotmail.com>
2020-01-27 16:52:20 +0100

    ya sale el background

--- background.js
+++ background.js
@@ -1,8 +1,8 @@
 class Background {
     constructor(ctx, w, h) {
       this._ctx = ctx;
-      this._width = screenWidth;
-      this._height = screenHeight;
+      this._width = w;
+      this._height = h;
   
       this._image = new Image();
       this._image.src = "./imagenes/background.png"
@@ -12,7 +12,7 @@ class Background {
     }
 
     draw(){
-      this._ctx.drawImage(this.image, this.posX, this.posY, this.width, this.height);
+      this._ctx.drawImage(this._image, this._posX, this._posY, this._width, this._height);
       this._ctx.drawImage(this._image,this._posX + this._width,this._posY,this._width,this._height);
 
     }
--- game.js
+++ game.js
@@ -1,36 +1,50 @@
-const game = {
-    constructor = {
-        canvas: undefined,
-        ctx : undefined,
-        width : 1000,
-        height: 800,
+let game = {
+    canvas : undefined,
+    ctx : undefined,
+    widht : undefined,
+    height : undefined,
+    FPS : 60,
+
+    init() {
+        this.canvas = document.getElementById("canvas");
+        this.ctx = this.canvas.getContext("2d");
+        this.setDimensions();
+        this.start();
+    },
+
+    start() {
+        this.reset();
+        this.interval = setInterval(()=>{
+            if(this.framesCounter>5000){
+                this.framesCounter = 0;
+            }
+            this.framesCounter++;
+            this.clear();
+            this.drawAll();
+        },1000/this.FPS);
 
     },
 
-    init(){
-        this.canvas = document.getElementById("canvas");
-        this.ctx = this.canvas.getContext("2d");
-        this.canvas.width=this.width;
-        this.canvas.height=this.height;
-        this.background = new Background(this.ctx,this.width,this.height);
-        this.background.draw();
+    setDimensions() {
+        this.width = window.innerWidth;
+        this.height = window.innerHeight;
+        this.canvas.width = this.width;
+        this.canvas.height = this.height;
+
     },
 
     drawAll() {
-        this.interval = setInterval(() => {
-            this.clear();
-            this.background.draw();
-      
-    },10);
-
+        this.background.draw()
     },
 
-    clear(){
-        this.ctx.clearRect(0,0,this.canvas.width,this.canvas.height);
+    reset() {
+        this.background = new Background(this.ctx, this.width, this.height, "./img/bg.png");
+        
     },
 
-};
+    clear() {
+        this.ctx.clearRect(0, 0, this.width, this.height);
+    },  
+    
 
-game.init();
-game.drawAll();
-
+}
